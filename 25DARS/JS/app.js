c9b45798cefffeb11ec72e5b99e7bb923e40c74b
Ozodbek Raxmatjonov <106082984+0z0dbek-coder@users.noreply.github.com>
2022-05-29 13:05:43 +0500

    Add files via upload

---
+++ 25DARS/JS/app.js
@@ -0,0 +1,16 @@
+var age = 16
+var name = 'Ozodbek'
+var surname = "Rahmatjonov"
+
+console.log(age);
+console.log(name);
+console.log(surname);
+
+
+document.write(name)
+document.write(surname)
+document.write(age)
+
+alert('Ozodbek')
+alert('Raxmatjonov')
+alert(16)
