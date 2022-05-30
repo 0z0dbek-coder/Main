146a8e58912ac149782359ff32d713e0befa6d43
Ozodbek Raxmatjonov <106082984+0z0dbek-coder@users.noreply.github.com>
2022-05-30 22:21:08 +0500

    Add files via upload
    Bu kod qaysi oy 30 kundan va qaysi oy 31 kundanligini  aniqlab beradi!

---
+++ Month/JS/app.js
@@ -0,0 +1,44 @@
+// Bilmoqchi bo'lgan oyingiz raqamini qavs ichiga kiriting!
+
+switch(2){
+    case 1:
+        console.log('Yanvar 31 kun');
+        break;
+    case 3:
+        console.log('Mart 31 kun');
+        break;
+    case 5:
+        console.log('May 31 kun');
+        break;
+    case 7:
+        console.log('Iyul 31 kun');
+        break;
+    case 8:
+        console.log('Avgust 31 kun');
+        break;
+    case 10:
+        console.log('Oktabr 31 kun');
+        break;
+    case 12:
+        console.log('Dekabr 31 kun');
+        break;
+
+    case 2:
+        console.log('Fevral 28 yoki 29 kun');
+        break;
+
+    case 4:
+        console.log('Aprel 30 kun');
+        break;
+    case 6:
+        console.log('Iyun 30 kun');
+        break;
+    case 9:
+        console.log('Sentabr 30 kun');
+        break;
+    case 11:       
+        console.log('Noyabr 30 kun');
+        break;
+    default:
+        console.log("Noto'g'ri oy");
+}
